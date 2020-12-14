a9b5385398aff0b38a739006e2b5481b719f5041
romab <roma.bruhanchik@gmail.com>
2020-12-14 16:19:54 +0300

    major changes

--- src/main/java/edu/epam/triangle/factory/impl/TriangleFactory.java
+++ src/main/java/edu/epam/triangle/factory/impl/TriangleFactory.java
@@ -7,7 +7,7 @@ import edu.epam.triangle.factory.ShapeFactory;
 public class TriangleFactory extends ShapeFactory {
 
     @Override
-    public Triangle createShape(Point...p) {
-        return new Triangle(p[0],p[1],p[2]);
+    public Triangle createShape(Point...points) {
+        return new Triangle(points[0],points[1],points[2]);
     }
 }
